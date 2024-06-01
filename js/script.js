223e7d92a41e761a8a76f422e24545bb9d59cc24
Albert Aptacy <albertaptacy@gmail.com>
2024-06-01 10:23:29 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-const buttonHobby = document.querySelector(".js-sectionButtonHobby");
-const hobby = document.querySelector(".js-sectionHobby");
 
-buttonHobby.addEventListener("click", () => {
-    hobby.classList.toggle("section__hobbyChange");
-});
+function hobbyChange() {
+    const buttonHobby = document.querySelector(".js-sectionButtonHobby");
+    const hobby = document.querySelector(".js-sectionHobby");
 
-const button = document.querySelector(".js-button");
-const body = document.querySelector(".js-body");
-const themeName = document.querySelector(".js-themeName");
+    buttonHobby.addEventListener("click", () => {
+        hobby.classList.toggle("section__hobbyChange");
+    });
+}
 
-button.addEventListener("click", () => {
-    body.classList.toggle("blueBackground");
-    body.classList.contains("blueBackground") ? themeName.innerText = "biały" : themeName.innerText = "błękitny";
+function blueWhiteBackground() {
+    const button = document.querySelector(".js-button");
+    const body = document.querySelector(".js-body");
+    const themeName = document.querySelector(".js-themeName");
 
-});
+    button.addEventListener("click", () => {
+        body.classList.toggle("blueBackground");
+        body.classList.contains("blueBackground") ? themeName.innerText = "biały" : themeName.innerText = "błękitny";
+    });
+}
+
+hobbyChange();
+blueWhiteBackground();
