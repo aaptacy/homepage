9255efcb652b66973d08b7b42ab22763e8272132
Albert Aptacy <albertaptacy@gmail.com>
2024-06-06 07:21:25 +0200

    function structurized

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
+{
+    const hobbyToggleBackground = () => {
+        const hobby = document.querySelector(".js-sectionHobby");
 
-function hobbyChange() {
-    const buttonHobby = document.querySelector(".js-sectionButtonHobby");
-    const hobby = document.querySelector(".js-sectionHobby");
-
-    buttonHobby.addEventListener("click", () => {
         hobby.classList.toggle("section__hobbyChange");
-    });
-}
+    };
 
-function blueWhiteBackground() {
-    const button = document.querySelector(".js-button");
-    const body = document.querySelector(".js-body");
-    const themeName = document.querySelector(".js-themeName");
+    const buttonHobby = document.querySelector(".js-sectionButtonHobby");
 
-    button.addEventListener("click", () => {
+    buttonHobby.addEventListener("click", hobbyToggleBackground);
+
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
+
         body.classList.toggle("blueBackground");
         body.classList.contains("blueBackground") ? themeName.innerText = "biały" : themeName.innerText = "błękitny";
-    });
-}
+    }
 
-hobbyChange();
-blueWhiteBackground();
+    const changeBodyBackgroundButton = document.querySelector(".js-button");
+
+    changeBodyBackgroundButton.addEventListener("click", toggleBackground);
+
+}
